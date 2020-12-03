60ca2b79e2434fe91894144018bc40622ef763eb
SuperAlina2020 <alinagithub@gmail.com>
2020-12-03 19:11:48 +0600

    added path 'check_order'

--- online_restaurant/restaurant/urls.py
+++ online_restaurant/restaurant/urls.py
@@ -4,11 +4,20 @@ from .views import *
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('menu',MenuView)
+router.register('check_order',MenuToOrderView)
 
 urlpatterns = [
 
     path('',include(router.urls)),
+    path('check_order/',include(router.urls)),
     path('orders/', OrderView.as_view(), name='orders'),
-    path('udorders/<int:pk>/', UDOrderView.as_view(), name='updatedeleteorders'),
+    path('update_orders/<int:pk>/', UpdateOrder.as_view(), name='updateorders'),
+    path('delete_orders/<int:pk>/',UDOrderView.as_view(),name='delete-order'),
+
+
+
+
+
+
 
 ]
