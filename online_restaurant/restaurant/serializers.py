85a0373b162e607fa5a16a0575ec8f01ea62e8ba
SuperAlina2020 <alinagithub@gmail.com>
2020-12-03 19:10:41 +0600

    created class MenuToOrderSerializer to get total_price of order

--- online_restaurant/restaurant/serializers.py
+++ online_restaurant/restaurant/serializers.py
@@ -9,18 +9,35 @@ class MenuSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    meal = MenuSerializer()
+
     class Meta:
         model = Order
         fields = '__all__'
 
 
 
-    def update(self, instance, validated_data):
-        meal = validated_data.pop('meal')
-        # instance.meal = validated_data.get('meal',instance.meal)
-        Order.objects.update(**meal)
-        return instance
+
+    # def update(self, instance, validated_data):
+    #     meal = validated_data.pop('meal')
+    #     # instance.meal = validated_data.get('meal',instance.meal)
+    #     Order.objects.update(**meal)
+    #     return instance
+
+class MenuToOrderSerializer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField('get_total_price')
+    class Meta:
+        model = MenuToOrder
+        fields = ['id', 'order', 'amounts', 'meal', 'total_price']
+
+    def get_total_price(self, order):
+        total_price = 0
+        check_order = MenuToOrder.objects.filter(order=order.order)
+        for order in check_order:
+            total_price += order.meal.price * order.amounts
+        return total_price
+
+
+
 
 
 
