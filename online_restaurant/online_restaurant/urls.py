74fea8895738e6e4aa0a30fa2e6d2421f1cb8909
SuperAlina2020 <alinagithub@gmail.com>
2020-12-04 19:18:31 +0600

    added path 'delivery'

--- online_restaurant/online_restaurant/urls.py
+++ online_restaurant/online_restaurant/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('online/',include('restaurant.urls')),
+    path('delivery/', include('delivery.urls')),
 ]
