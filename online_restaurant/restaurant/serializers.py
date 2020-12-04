4e0a3aa7d5d1f0a2cf19abe75e5bedbf2b844c5c
SuperAlina2020 <alinagithub@gmail.com>
2020-12-04 19:17:34 +0600

    added sales for total price

--- online_restaurant/restaurant/serializers.py
+++ online_restaurant/restaurant/serializers.py
@@ -25,16 +25,23 @@ class OrderSerializer(serializers.ModelSerializer):
 
 class MenuToOrderSerializer(serializers.ModelSerializer):
     total_price = serializers.SerializerMethodField('get_total_price')
+
     class Meta:
         model = MenuToOrder
-        fields = ['id', 'order', 'amounts', 'meal', 'total_price']
+        fields = ['id', 'order', 'amounts', 'meal', 'total_price', 'sales']
 
     def get_total_price(self, order):
         total_price = 0
-        check_order = MenuToOrder.objects.filter(order=order.order)
-        for order in check_order:
-            total_price += order.meal.price * order.amounts
-        return total_price
+
+        if self.data['sales']:
+            check_order = MenuToOrder.objects.filter(order=order.order)
+            for order in check_order:
+                total_price += order.meal.price * order.amounts
+                sales = order.total_price - order.total_price * order.percent // 100
+                return sales
+            return total_price
+
+
 
 
 
