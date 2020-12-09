1d4136aa9086c79799a8f2419bfc67cf6bb1e994
SuperAlina2020 <alinagithub@gmail.com>
2020-12-09 16:15:32 +0600

    added path 'register'

--- online_restaurant/delivery/urls.py
+++ online_restaurant/delivery/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register('', CustomerRegisterViewset)
 
 urlpatterns = [
+    path('register/',include(router.urls)),
     path('delivery_orders/', OrderToDeliveryView.as_view(), name='delivery-order'),
 
 ]
