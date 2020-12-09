06bb9030e1f6a978fd9b2dd8dd6358448114b902
SuperAlina2020 <alinagithub@gmail.com>
2020-12-09 16:11:05 +0600

    added MenuToOrder

--- online_restaurant/restaurant/admin.py
+++ online_restaurant/restaurant/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register([Menu, Order, Table, Sales])
+admin.site.register([Menu, Order, Table, Sales, MenuToOrder])
