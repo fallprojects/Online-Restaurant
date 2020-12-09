823d85556cca9cdd74c9242b3e8f885e6f41acf3
SuperAlina2020 <alinagithub@gmail.com>
2020-12-09 16:15:50 +0600

    added path 'register' and 'login'

--- online_restaurant/restaurant/urls.py
+++ online_restaurant/restaurant/urls.py
@@ -5,19 +5,17 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('menu',MenuView)
 router.register('check_order',MenuToOrderView)
+router.register('register', RegisterLoginView)
+
 
 urlpatterns = [
-
     path('',include(router.urls)),
     path('check_order/',include(router.urls)),
     path('orders/', OrderView.as_view(), name='orders'),
     path('update_orders/<int:pk>/', UpdateOrder.as_view(), name='updateorders'),
     path('delete_orders/<int:pk>/',UDOrderView.as_view(),name='delete-order'),
-
-
-
-
-
+    path('register/', include(router.urls), name='register'),
+    path('login/', AccountLoginView.as_view(), name='login'),
 
 
 ]
